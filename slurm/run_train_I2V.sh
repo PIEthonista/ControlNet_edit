75c3fbc8f5b4606725d4038996a43acded8ef078
Chun Chet Ng <ngchunchet95@gmail.com>
2023-10-15 23:33:28 +0800

    latest

--- slurm/run_train_I2V.sh
+++ slurm/run_train_I2V.sh
@@ -9,8 +9,9 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=gohyixian456@gmail.com
 
-# 528322
-python custom_train.py --train_dataset_cond_dir /work/u5832291/datasets/LLVIP/infrared/train --train_dataset_target_dir /work/u5832291/datasets/LLVIP/visible/train --val_dataset_cond_dir /work/u5832291/datasets/LLVIP/infrared/test --val_dataset_target_dir /work/u5832291/datasets/LLVIP/visible/test --batch_size 4 --val_batch_size 4 --num_workers 4 --image_size 256 256 --resume_path /work/u5832291/yixian/ControlNet_edit/models/control_sd15_ini.ckpt --cldm_model_config /work/u5832291/yixian/ControlNet_edit/models/cldm_v15.yaml --sd_locked True --only_mid_control False --max_epochs 1000
+# 528322 (failed)
+# 528509
+python custom_train.py --train_dataset_cond_dir /work/u5832291/datasets/LLVIP/infrared/train --train_dataset_target_dir /work/u5832291/datasets/LLVIP/visible/train --val_dataset_cond_dir /work/u5832291/datasets/LLVIP/infrared/test --val_dataset_target_dir /work/u5832291/datasets/LLVIP/visible/test --batch_size 4 --val_batch_size 4 --num_workers 4 --image_size 256 256 --resume_path /work/u5832291/yixian/ControlNet_edit/models/control_sd15_ini.ckpt --cldm_model_config /work/u5832291/yixian/ControlNet_edit/models/cldm_v15.yaml --sd_locked True --only_mid_control False --max_epochs 5
 
 # python custom_train.py 
 # --train_dataset_cond_dir /work/u5832291/datasets/LLVIP/infrared/train
@@ -25,4 +26,4 @@ python custom_train.py --train_dataset_cond_dir /work/u5832291/datasets/LLVIP/in
 # --cldm_model_config /work/u5832291/yixian/ControlNet_edit/models/cldm_v15.yaml
 # --sd_locked True
 # --only_mid_control False
-# --max_epochs 1000
+# --max_epochs 5
--- slurm/run_train_V2I.sh
+++ slurm/run_train_V2I.sh
@@ -9,8 +9,9 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=gohyixian456@gmail.com
 
-# 528323
-python custom_train.py --train_dataset_cond_dir /work/u5832291/datasets/LLVIP/visible/train --train_dataset_target_dir /work/u5832291/datasets/LLVIP/infrared/train --val_dataset_cond_dir /work/u5832291/datasets/LLVIP/visible/test --val_dataset_target_dir /work/u5832291/datasets/LLVIP/infrared/test --batch_size 4 --val_batch_size 4 --num_workers 4 --image_size 256 256 --resume_path /work/u5832291/yixian/ControlNet_edit/models/control_sd15_ini.ckpt --cldm_model_config /work/u5832291/yixian/ControlNet_edit/models/cldm_v15.yaml --sd_locked True --only_mid_control False --max_epochs 1000
+# 528323 (failed)
+# 528508
+python custom_train.py --train_dataset_cond_dir /work/u5832291/datasets/LLVIP/visible/train --train_dataset_target_dir /work/u5832291/datasets/LLVIP/infrared/train --val_dataset_cond_dir /work/u5832291/datasets/LLVIP/visible/test --val_dataset_target_dir /work/u5832291/datasets/LLVIP/infrared/test --batch_size 4 --val_batch_size 4 --num_workers 4 --image_size 256 256 --resume_path /work/u5832291/yixian/ControlNet_edit/models/control_sd15_ini.ckpt --cldm_model_config /work/u5832291/yixian/ControlNet_edit/models/cldm_v15.yaml --sd_locked True --only_mid_control False --max_epochs 5
 
 # python custom_train.py 
 # --train_dataset_cond_dir /work/u5832291/datasets/LLVIP/visible/train
@@ -25,4 +26,4 @@ python custom_train.py --train_dataset_cond_dir /work/u5832291/datasets/LLVIP/vi
 # --cldm_model_config /work/u5832291/yixian/ControlNet_edit/models/cldm_v15.yaml
 # --sd_locked True
 # --only_mid_control False
-# --max_epochs 1000
+# --max_epochs 5
