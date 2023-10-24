2e0c4a7f033ddb6fc688dc86e0604bfa4f2adddd
Chun Chet Ng <ngchunchet95@gmail.com>
2023-10-24 10:21:22 +0800

    twcc update

--- slurm/run_train_I2V.sh
+++ slurm/run_train_I2V.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 #SBATCH -A MST111109
-#SBATCH -J I2V_training_20231021_sd_unlocked_epoch_10
+#SBATCH -J I2V_training_20231024_sd_unlocked_epoch_10
 #SBATCH -p gp4d
-#SBATCH -e train_I2V_training_20231021_sd_unlocked_epoch_10.txt
+#SBATCH -e train_I2V_training_20231024_sd_unlocked_epoch_10.txt
 #SBATCH -c 4
 #SBATCH --gres=gpu:1
 #SBATCH --mail-type=ALL
@@ -13,8 +13,9 @@
 # 528509 (failed)
 # 528958 (done, epoch 5)
 # 531204 (done, epoch 7)
-# 531484 (running, epoch 10, sd unlocked)
-python custom_train.py  --train_dataset_cond_dir /work/u5832291/datasets/LLVIP/infrared/train --train_dataset_target_dir /work/u5832291/datasets/LLVIP/visible/train --val_dataset_cond_dir /work/u5832291/datasets/LLVIP/infrared/test --val_dataset_target_dir /work/u5832291/datasets/LLVIP/visible/test --batch_size 4 --val_batch_size 4 --num_workers 4 --image_size 256 256 --resume_path /work/u5832291/yixian/ControlNet_edit/models/control_sd15_ini.ckpt --cldm_model_config /work/u5832291/yixian/ControlNet_edit/models/cldm_v15.yaml --sd_locked False --only_mid_control False --max_epochs 10 --save_ckpt_every_n_epoch 1 --save_ckpt_dir /work/u5832291/yixian/ControlNet_edit/experiments/20231021_I2V_sd_unlocked_epoch_10 --save_ckpt_filename 20231021_I2V  --wandb_project ControlNet-edit --wandb_run_name I2V_training_20231021
+# 531484 (done, epoch 10, sd unlocked)
+# 532080 (running, epoch 10, sd unlocked, ori code)
+python custom_train.py  --train_dataset_cond_dir /work/u5832291/datasets/LLVIP/infrared/train --train_dataset_target_dir /work/u5832291/datasets/LLVIP/visible/train --val_dataset_cond_dir /work/u5832291/datasets/LLVIP/infrared/test --val_dataset_target_dir /work/u5832291/datasets/LLVIP/visible/test --batch_size 4 --val_batch_size 4 --num_workers 4 --image_size 256 256 --resume_path /work/u5832291/yixian/ControlNet_edit/models/control_sd15_ini.ckpt --cldm_model_config /work/u5832291/yixian/ControlNet_edit/models/cldm_v15.yaml --sd_locked False --only_mid_control False --max_epochs 10 --save_ckpt_every_n_epoch 1 --save_ckpt_dir /work/u5832291/yixian/ControlNet_edit/experiments/20231024_I2V_sd_unlocked_epoch_10_ORI_CODE --save_ckpt_filename 20231024_I2V  --wandb_project ControlNet-edit --wandb_run_name I2V_training_20231024
 
 # python custom_train.py 
 # --train_dataset_cond_dir /work/u5832291/datasets/LLVIP/infrared/train
@@ -31,7 +32,7 @@ python custom_train.py  --train_dataset_cond_dir /work/u5832291/datasets/LLVIP/i
 # --only_mid_control False
 # --max_epochs 10
 # --save_ckpt_every_n_epoch 1
-# --save_ckpt_dir /work/u5832291/yixian/ControlNet_edit/experiments/20231021_I2V
-# --save_ckpt_filename 20231021_I2V
+# --save_ckpt_dir /work/u5832291/yixian/ControlNet_edit/experiments/20231024_I2V
+# --save_ckpt_filename 20231024_I2V
 # --wandb_project ControlNet-edit
-# --wandb_run_name I2V_training_20231021
+# --wandb_run_name I2V_training_20231024
--- slurm/run_train_V2I.sh
+++ slurm/run_train_V2I.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 #SBATCH -A MST111109
-#SBATCH -J V2I_training_20231021_sd_unlocked_epoch_10
+#SBATCH -J V2I_training_20231024_sd_unlocked_epoch_10
 #SBATCH -p gp4d
-#SBATCH -e train_V2I_training_20231021_sd_unlocked_epoch_10.txt
+#SBATCH -e train_V2I_training_20231024_sd_unlocked_epoch_10.txt
 #SBATCH -c 4
 #SBATCH --gres=gpu:1
 #SBATCH --mail-type=ALL
@@ -11,10 +11,11 @@
 
 # 528323 (failed)
 # 528508 (failed)
-# 528950 (done epoch 5)
-# 531205 (done epoch 7)
-# 531485 (running, epoch 10, sd unlocked)
-python custom_train.py  --train_dataset_cond_dir /work/u5832291/datasets/LLVIP/visible/train --train_dataset_target_dir /work/u5832291/datasets/LLVIP/infrared/train --val_dataset_cond_dir /work/u5832291/datasets/LLVIP/visible/test --val_dataset_target_dir /work/u5832291/datasets/LLVIP/infrared/test --batch_size 4 --val_batch_size 4 --num_workers 4 --image_size 256 256 --resume_path /work/u5832291/yixian/ControlNet_edit/models/control_sd15_ini.ckpt --cldm_model_config /work/u5832291/yixian/ControlNet_edit/models/cldm_v15.yaml --sd_locked False --only_mid_control False --max_epochs 10 --save_ckpt_every_n_epoch 1 --save_ckpt_dir /work/u5832291/yixian/ControlNet_edit/experiments/20231021_V2I_sd_unlocked_epoch_10 --save_ckpt_filename 20231021_V2I --wandb_project ControlNet-edit --wandb_run_name V2I_training_20231021
+# 528950 (done, epoch 5)
+# 531205 (done, epoch 7)
+# 531485 (done, epoch 10, sd unlocked)
+# 532081 (running, epoch 10, sd unlocked, ori code)
+python custom_train.py  --train_dataset_cond_dir /work/u5832291/datasets/LLVIP/visible/train --train_dataset_target_dir /work/u5832291/datasets/LLVIP/infrared/train --val_dataset_cond_dir /work/u5832291/datasets/LLVIP/visible/test --val_dataset_target_dir /work/u5832291/datasets/LLVIP/infrared/test --batch_size 4 --val_batch_size 4 --num_workers 4 --image_size 256 256 --resume_path /work/u5832291/yixian/ControlNet_edit/models/control_sd15_ini.ckpt --cldm_model_config /work/u5832291/yixian/ControlNet_edit/models/cldm_v15.yaml --sd_locked False --only_mid_control False --max_epochs 10 --save_ckpt_every_n_epoch 1 --save_ckpt_dir /work/u5832291/yixian/ControlNet_edit/experiments/20231024_V2I_sd_unlocked_epoch_10_ORI_CODE --save_ckpt_filename 20231024_V2I --wandb_project ControlNet-edit --wandb_run_name V2I_training_20231024
 
 # python custom_train.py 
 # --train_dataset_cond_dir /work/u5832291/datasets/LLVIP/visible/train
@@ -31,7 +32,7 @@ python custom_train.py  --train_dataset_cond_dir /work/u5832291/datasets/LLVIP/v
 # --only_mid_control False
 # --max_epochs 10
 # --save_ckpt_every_n_epoch 1
-# --save_ckpt_dir /work/u5832291/yixian/ControlNet_edit/experiments/20231021_V2I
-# --save_ckpt_filename 20231021_V2I
+# --save_ckpt_dir /work/u5832291/yixian/ControlNet_edit/experiments/20231024_V2I
+# --save_ckpt_filename 20231024_V2I
 # --wandb_project ControlNet-edit
-# --wandb_run_name V2I_training_20231021
+# --wandb_run_name V2I_training_20231024
