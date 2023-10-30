4e50a6f9deff7de8cec5ec7f12a155adeae0a19f
Chun Chet Ng <ngchunchet95@gmail.com>
2023-10-30 18:49:08 +0800

    eval for trained ori code tests

---
+++ slurm/EVAL_inf_20231024_V2I_sd_unlocked_epoch_10_ORI_CODE_w_txtprompt_step200_ckptlast.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+#SBATCH -A MST111109
+#SBATCH -J eval_std_fid
+#SBATCH -p gp4d
+#SBATCH -e EVAL_inf_20231024_V2I_sd_unlocked_epoch_10_ORI_CODE_w_txtprompt_step200_ckptlast.txt
+#SBATCH -c 4
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=gohyixian456@gmail.com
+
+# 535627
+srun python /work/u5832291/yixian/palette_scene2scene_rec/eval_std_fid.py -s /work/u5832291/datasets/LLVIP/infrared/test -d /work/u5832291/yixian/ControlNet_edit/experiments/20231024_V2I_sd_unlocked_epoch_10_ORI_CODE_w_txtprompt/inf_outputs_step_200_ckpt_last
